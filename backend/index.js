6b6f414657f16656e995a8f0946b76b450f87742
parvashah305 <parvashah305@gmail.com>
2025-03-30 16:50:31 +0530

    cors issue solved

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT=4000
 app.use(express.json())
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://carrer-wave.vercel.app",
       credentials: true, // Allow sending cookies from the frontend
     })
   );
